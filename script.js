e62f9a6aae95e6eb9ad76a6153aa3ba1369e06fb
Vitudsg <vtvitucosta@gmail.com>
2026-03-24 10:09:15 -0300

    finew

--- script.js
+++ script.js
@@ -1,65 +1,11 @@
-let humanScore = 0;
-let computerScore = 0;
+let n = prompt("Enter a number: ");
 
-function getComputerChoice() {
-    let drawnNumber = Math.floor(Math.random() * 3) + 1;
+nextPrime:
+for (let i = 2; i <= n; i++) { // for each i...
 
-    if (drawnNumber === 1) {
-        return "rock";
+  for (let j = 2; j < i; j++) { // look for a divisor..
+    if (i % j == 0) continue nextPrime; // not a prime, go next i
+  }
 
-    }
-    else if (drawnNumber === 2) {
-        return "paper"
-
-    }else {
-        return "scissors"
-    }
-
+  alert( i ); // a prime
 }
-
-function getHumanChoice() {
-    let choice = prompt("Please, enter rock, paper, or scissors. ")
-
-    return choice.toLowerCase();
-}
-
-function playRound(humanChoice, computerChoice) {
-
-    if (humanChoice === "scissors" && computerChoice === "paper" ||
-        humanChoice === "paper" && computerChoice === "rock" ||
-        humanChoice === "rock" && computerChoice === "scissors"
-    ) {
-        humanScore++;
-        console.log("You win this round! " +humanChoice+ " BEATS " +computerChoice)
-    }else {
-        computerScore++;
-        console.log("You lose this round! " +computerChoice+ " BEATS " + humanChoice )
-    }
-
-}
-
-function playGame() {
-
-    for(let i = 1; i <= 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    if(humanScore > computerScore) {
-        return console.log("You win this match! you have " +humanScore+ " points")
-
-    }
-    else if(computerScore > humanScore) {
-        return console.log("You lose this match! computer have " +computerScore+ " points")
-    }
-    else {
-        return console.log("This is a DRAW match!")
-    }
-    
-}
-
-playGame();
-
-console.log(humanScore)
-console.log(computerScore)
