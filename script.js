c14f4963e1e5c61b69314c389278b9b25c0fadf8
Vitudsg <vtvitucosta@gmail.com>
2026-03-30 19:25:34 -0300

    Interface add

--- script.js
+++ script.js
@@ -1,65 +1,82 @@
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+
+const btnRock = document.querySelector("#btnRock");
+const btnPaper = document.querySelector("#btnPaper");
+const btnScissors = document.querySelector("#btnScissors");
+const winRound = document.querySelector("#winRound");
+const winMessage = document.querySelector("#winMessage");
+const scoreMessage = document.querySelector("#scoreMessage");
+
+scoreMessage.style.fontSize = "20px";
+scoreMessage.style.marginBottom = "25px";
+
+winMessage.style.fontSize = "25px";
+winMessage.style.fontWeight = "bold";
+winMessage.style.marginBottom = "25px";
 
 function getComputerChoice() {
     let drawnNumber = Math.floor(Math.random() * 3) + 1;
-
+    
     if (drawnNumber === 1) {
         return "rock";
-
+        
     }
     else if (drawnNumber === 2) {
         return "paper"
-
+        
     }else {
         return "scissors"
     }
-
+    
 }
 
-function getHumanChoice() {
-    let choice = prompt("Please, enter rock, paper, or scissors. ")
 
-    return choice.toLowerCase();
-}
-
-function playRound(humanChoice, computerChoice) {
-
+function playGame(humanChoice, computerChoice) {
+    
     if (humanChoice === "scissors" && computerChoice === "paper" ||
         humanChoice === "paper" && computerChoice === "rock" ||
         humanChoice === "rock" && computerChoice === "scissors"
     ) {
         humanScore++;
         console.log("You win this round! " +humanChoice+ " BEATS " +computerChoice)
+        winRound.textContent = "You win this round! " +humanChoice+ " BEATS " +computerChoice;
+        scoreMessage.textContent = "Your score: " +humanScore+ " Computer score: " +computerScore;
     }else {
         computerScore++;
         console.log("You lose this round! " +computerChoice+ " BEATS " + humanChoice )
+        winRound.textContent = "You lose this round! " +computerChoice+ " BEATS " + humanChoice;
+        scoreMessage.textContent = "Your score: " +humanScore+ " Computer score: " +computerScore;
     }
 
+        if (humanScore === 5) {
+            winMessage.textContent = "Congratulations! You won the game!";
+            humanScore = 0;
+            computerScore = 0;
+        }else if (computerScore === 5) {
+            winMessage.textContent = "Sorry! You lost the game!";
+            humanScore = 0;
+            computerScore = 0;
+        }
 }
 
-function playGame() {
+btnRock.addEventListener("click", () => {
+    humanChoice = "rock";
+    playGame(humanChoice, getComputerChoice())
+});
 
-    for(let i = 1; i <= 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
+btnPaper.addEventListener("click", () => {
+    humanChoice = "paper";
+    playGame(humanChoice   , getComputerChoice());
+});
 
-    if(humanScore > computerScore) {
-        return console.log("You win this match! you have " +humanScore+ " points")
+btnScissors.addEventListener("click", () => {
+    humanChoice = "scissors";
+    playGame(humanChoice, getComputerChoice());
+});
 
-    }
-    else if(computerScore > humanScore) {
-        return console.log("You lose this match! computer have " +computerScore+ " points")
-    }
-    else {
-        return console.log("This is a DRAW match!")
-    }
-    
-}
 
-playGame();
 
 console.log(humanScore)
 console.log(computerScore)
